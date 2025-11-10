e9165a8bb52b2a1218ef7579b9c66a6abfb3b15d
HonT1 <tikhon.zavialov@studytafensw.edu.au>
2025-11-11 09:39:55 +1100

    Change server port from 5000 to 10000

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ connectDB();
 //routes
 app.use("/api/orders", orderRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
